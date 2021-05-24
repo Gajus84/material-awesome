15ca53379b98d83f3eca033897938888e32ef949
Gajus84 <44810947+Gajus84@users.noreply.github.com>
2021-05-24 08:01:51 +0200

    Add Git-Folder

---
+++ widget/xdg-folders/git.lua
@@ -0,0 +1,64 @@
+local wibox = require('wibox')
+local awful = require('awful')
+local naughty = require('naughty')
+local gears = require('gears')
+
+local clickable_container = require('widget.clickable-container')
+local dpi = require('beautiful').xresources.apply_dpi
+
+local config_dir = gears.filesystem.get_configuration_dir()
+local widget_icon_dir = config_dir .. 'widget/xdg-folders/icons/'
+
+local create_widget = function()
+	local dl_widget = wibox.widget {
+		{
+			image = widget_icon_dir .. 'folder-git.svg',
+			resize = true,
+			widget = wibox.widget.imagebox
+		},
+		layout = wibox.layout.align.horizontal
+	}
+
+	local git_button = wibox.widget {
+		{
+			dl_widget,
+			margins = dpi(10),
+			widget = wibox.container.margin
+		},
+		widget = clickable_container
+	}
+
+	git_button:buttons(
+		gears.table.join(
+			awful.button(
+				{},
+				1,
+				nil,
+				function()
+					awful.spawn.with_shell('xdg-open $(xdg-user-dir GIT)')
+                local focused = awful.screen.focused()
+				local tag = focused.tags[3]
+				if tag then
+					tag:view_only()
+				end
+				end
+			)
+		)
+	)
+
+	awful.tooltip(
+		{
+			objects = {git_button},
+			mode = 'outside',
+			align = 'right',
+			text = 'Git',
+			margin_leftright = dpi(8),
+			margin_topbottom = dpi(8),
+			preferred_positions = {'top', 'bottom', 'right', 'left'}
+		}
+	)
+
+	return git_button
+end
+
+return create_widget
